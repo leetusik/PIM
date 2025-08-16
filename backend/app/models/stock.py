380a5485713addf0fd271729af6f942800672e44
leetusik <leetusik@gmail.com>
2025-08-16 19:59:06 +0900

    feat: basic stock and daily_price model

--- backend/app/models/stock.py
+++ backend/app/models/stock.py
@@ -1,5 +1,8 @@
 from app.db.base import Base
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql.schema import ForeignKey
+from sqlalchemy.types import Date, Float
 
 
 class Stock(Base):
@@ -9,3 +12,21 @@ class Stock(Base):
     name = Column(String, index=True)
     market = Column(String, index=True)
     ticker = Column(String, index=True)
+    dart_code = Column(String, index=True)
+
+    daily_prices = relationship("daily_price", back_populates="stock")
+
+
+class daily_price(Base):
+    __tablename__ = "daily_price"
+
+    id = Column(Integer, primary_key=True, index=True)
+    stock_id = Column(Integer, ForeignKey("stocks.id"))
+    date = Column(Date, index=True)
+    open = Column(Float, index=True)
+    high = Column(Float, index=True)
+    low = Column(Float, index=True)
+    close = Column(Float, index=True)
+    volume = Column(Integer, index=True)
+
+    stock = relationship("Stock", back_populates="daily_prices")
