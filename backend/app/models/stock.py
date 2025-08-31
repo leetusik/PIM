6e864461f5ee562c5d28b38079cdebf05a17f921
수강 이 <leetusik@gmail.com>
2025-08-31 21:23:06 +0900

    feat: add DailyPrice ma technical fields

--- backend/app/models/stock.py
+++ backend/app/models/stock.py
@@ -6,7 +6,7 @@ from sqlalchemy import Enum as SQLAlchemyEnum
 from sqlalchemy import Integer, String
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql.schema import ForeignKey, UniqueConstraint
-from sqlalchemy.types import Date, Float
+from sqlalchemy.types import Boolean, Date, Float
 
 
 class Market(enum.Enum):
@@ -38,12 +38,17 @@ class DailyPrice(Base):
     close = Column(Float, index=True)
     volume = Column(Float, index=True)
     value = Column(Float, index=True)
+
     ma_50 = Column(Float, index=True)
     ma_150 = Column(Float, index=True)
     ma_200 = Column(Float, index=True)
+    ma_200_20d_ago = Column(Float, index=True)
+    is_ma_200_bullish = Column(Boolean, index=True)
+    week_52_high = Column(Float, index=True)
+    is_near_52w_high = Column(Boolean, index=True)
+    week_52_low = Column(Float, index=True)
+    is_above_52w_low = Column(Boolean, index=True)
 
     stock = relationship("Stock", back_populates="daily_prices")
 
-    __table_args__ = (
-        UniqueConstraint('stock_id', 'date', name='unique_stock_date'),
-    )
+    __table_args__ = (UniqueConstraint("stock_id", "date", name="unique_stock_date"),)
